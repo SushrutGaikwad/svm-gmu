f262941b42162e6b0fa1dedbe9558c7f4f7f1828
Sushrut Gaikwad <sushrut.ishwargaikwad@students.iiserpune.ac.in>
2026-03-25 04:26:37 -0400

    feat: expose public API from package init

--- src/svm_gmu/__init__.py
+++ src/svm_gmu/__init__.py
@@ -1,2 +1,18 @@
-def main() -> None:
-    print("Hello from svm-gmu!")
+"""
+SVM-GMU: Support Vector Machines with Gaussian Mixture Sample Uncertainty.
+
+A classifier that accounts for per-sample non-Gaussian uncertainty
+modelled as Gaussian mixtures, extending the SVM-GSU framework.
+"""
+
+from .classifier import SVMGMU
+from .data import generate_gmm_dataset, validate_gmm_dataset
+from .loss import expected_hinge_loss_gaussian, expected_hinge_loss_gmm
+
+__all__ = [
+    "SVMGMU",
+    "validate_gmm_dataset",
+    "generate_gmm_dataset",
+    "expected_hinge_loss_gaussian",
+    "expected_hinge_loss_gmm",
+]
